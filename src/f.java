33775bb6bbd02ba210e0f20e1e037c8943b35a67
NurkyzZhakshylykova <57569993+NurkyzZhakshylykova@users.noreply.github.com>
2019-11-09 16:16:24 -0600

    begining

--- src/f.java
+++ src/f.java
@@ -1,6 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class f {
     public static void main(String[] args) {
 
         System.out.println("hello world");
+        System.out.println("Hi");
     }
+
 }
